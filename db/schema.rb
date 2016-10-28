df19a20ff11c6753cd2cb0d711c352e51262d6e6
Hamit YAY <hamityay@hotmail.com>
2016-10-28 14:50:15 +0300

    Added is_active column to Customer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026170115) do
+ActiveRecord::Schema.define(version: 20161028114540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,13 +76,13 @@ ActiveRecord::Schema.define(version: 20161026170115) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "customer", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+  create_table "customers", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20161026170115) do
     t.string   "phone"
     t.string   "officer"
     t.integer  "total"
+    t.boolean  "is_active",              default: false
   end
 
   add_index "customers", ["email"], name: "index_customers_on_email", unique: true, using: :btree
